649c5f31255b8c26776c09e6d015a1da0ab7ebde
Jochem Harteveld <jochemh83@gmail.com>
2023-04-17 11:58:02 +0200

    added jokers

--- src/routes/(site)/jokers/+page.svelte
+++ src/routes/(site)/jokers/+page.svelte
@@ -1,7 +1,160 @@
-<h1>[Hier komt later meer info]</h1>
+<div class="container">
+	<div class="day">
+		<h1>DAGJOKERS</h1>
+		<p>
+			Elke dag van de DCWeek zal er een jokerdagopdracht verschijnen, met uitzondering van donderdag
+			en vrijdag. Een dagjoker kan positief of negatief zijn. Een positieve dagjoker geeft 10, 15,
+			en 20 procent extra punten aan ieders eigen dispuut voor een specifieke activiteit van de
+			daaropvolgende dag. Een negatieve dagjoker geeft -10, -15, en -20 procent minder punten aan
+			een ander dispuut naar keuze voor een specifieke activiteit van de daaropvolgende dag.
+			<br /><br />
+
+			De joker-dagopdracht dient voor 22u00 s’avonds op de betreffende dag ingeleverd te worden via
+			de mail. Aan het eind van de dag beslist de DCW-redactie de top 3 disputen die de
+			joker-dagopdracht het best hebben uitgevoerd, gebaseerd op originaliteit, uitvoering en stijl.
+			Wanneer een dispuut een joker verdient, wordt deze de volgende dag direct ingezet. Het dispuut
+			mag kiezen op welke van de twee activiteiten de joker wordt ingezet.<br /><br />
+
+			De eerste plaats krijgt hierbij de joker van 20 procent, de tweede plaats de joker van 15
+			procent, en de derde plaats de joker van 10 procent. Elk dispuut kan OF een positieve joker op
+			zichzelf inzetten, OF een negatieve joker op een ander dispuut. Er kan op een dispuut maar één
+			joker worden ingezet, dus als een ander dispuut een negatieve joker op jouw dispuut heeft
+			ingezet, kan je voor die activiteit geen positieve joker meer op jezelf inzetten en andersom.
+			Het inzetten van de jokers gaat op volgorde van 1e, 2e en 3e plaats.<br /><br />
+
+			Als een dispuut een negatieve dagjoker wilt inzetten dienen zij - via dezelfde mail als die
+			waarin zij de joker-dagopdracht zelf inleveren - een top 3 door te geven van de disputen op
+			wie zijn hun joker in willen zetten. Dit voorkomt een systeemstoring. <br /><br />
+
+			Op donderdag is er geen jokerdagopdracht, omdat er geen dagjokers ingezet kunnen worden op de
+			vrijdag. Op vrijdag is er vanzelfsprekend ook geen jokerdagopdracht.<br /><br />
+		</p>
+		<h3>Kortom:</h3>
+		<ul>
+			<li>Op maandag, dinsdag en woensdag is één joker te verdienen via een jokerdagopdracht.</li>
+			<li>
+				Het resultaat van de uitgevoerde jokerdagopdracht moet vóór 22u00 op de betreffende dag
+				ingeleverd worden via de DCW-mail.
+			</li>
+			<li>
+				De DCW-redactie beoordeelt welke disputen met hun uitvoering van de jokerdagopdracht op de
+				1e, 2e of 3e plek zijn geëindigd.
+			</li>
+			<li>
+				De joker voor de 1e plek is 20% waard, de joker voor de 2e plek 15% en de joker voor de 3e
+				plek 10%.
+			</li>
+			<li>
+				Disputen mogen zelf kiezen of ze de gewonnen joker als positieve joker op zichzelf of als
+				negatieve joker op een ander dispuut naar keuze inzetten. Dit moeten ze vermelden in de mail
+				bij het inleveren van de jokerdagopdracht. Wanneer de disputen een negatieve joker in willen
+				zetten, moeten ze een voorkeurs top 3 disputen doorgeven waarop ze de joker in willen
+				zetten.
+			</li>
+			<li>
+				Wanneer een dispuut een joker verdient, wordt deze de volgende dag direct ingezet. Het
+				dispuut mag zelf kiezen op welke van de twee activiteiten de joker wordt ingezet. Dit moeten
+				ze vermelden in de mail bij het inleveren van de jokerdagopdracht.
+			</li>
+			<li>
+				Er kan per activiteit maar één joker op een dispuut worden ingezet. Voorrang gaat daarbij
+				naar disputen die hoger zijn geëindigd (eerst de 1e plaats, dan de 2e plaats en daarna de 3e
+				plaats).
+			</li>
+		</ul>
+
+        <h2>Jokeroverzicht</h2>
+
+		<h3>DAGJOKERS MAANDAG</h3>
+		<ol>
+			<li>
+				<span>Dierenval:</span> Vang jij eten om je dispuut te helpen met overleven? Of kies je ervoor
+				om een ander dispuut in de val te lokken?
+			</li>
+			<li>
+				<span>Zakmes:</span>Gebruik jij je mes om genoeg hout mee te snijden om een schuilplaats te
+				maken voor je hele dispuut? Of kies je ervoor om een ander dispuut in de rug te steken?
+			</li>
+			<li>
+				<span>Aansteker:</span>Steek jij met jouw aansteker genoeg hout aan om een lekker knus
+				kampvuur te creëren voor je dispuut? Of besluit je om in plaats daarvan simpelweg het kamp
+				van een ander dispuut in de fik te steken?
+			</li>
+		</ol>
+		<h3>DAGJOKERS Dinsdag</h3>
+		<ol>
+			<li>
+				<span>Levens:</span>Laat jij het spel langer duren door je dispuut extra levens te geven? Of
+				kies je er liever voor om de levens van een ander weg te stelen?
+			</li>
+			<li>
+				<span>Oplader:</span>Gebruik jij je oplader om de energie binnen je eigen dispuut weer op te
+				laden? Of trek je de stekker van een ander dispuut eruit?
+			</li>
+			<li>
+				<span>Inktpotje:</span>Schrijf jij de mooiste liefdesbrief ooit om aan jouw dispuut te laten
+				weten hoeveel ze voor je betekenen? Of giet je in plaats daarvan liever het potje leeg over
+				de brief van een ander dispuut?
+			</li>
+		</ol>
+		<h3>DAGJOKERS Woensdag</h3>
+		<ol>
+			<li>
+				<span>Reddingsboot:</span>Red jij je eigen dispuut van verdrinking? Of laat je Jack in de
+				zee te laten vallen om nooit meer terug te keren?
+			</li>
+			<li>
+				<span>Flare gun:</span>Signaleer jij met je flare gun het SOS-signaal in de lucht om de
+				aandacht van een voorbij varende boot te trekken? Of richt je deze liever op een ander
+				dispuut?
+			</li>
+			<li>
+				<span>Sekshand:</span>Heb je samen met je dispuut nog een laatste niet-te-vergeten wilde
+				avond? Of spank je een ander dispuut?
+			</li>
+		</ol>
+	</div>
+
+	<div class="week">
+		<h1>WEEKJOKERS</h1>
+		<p>
+			Naast dagjokers, is er ook een weekjoker. Deze weekjoker kan door elk dispuut verdiend worden
+			door het vervullen van de joker-weekopdracht en dient voor donderdagavond 22u00 s’avonds
+			ingeleverd te worden. Dit is een grotere weekopdracht die, wanneer goed uitgevoerd, een joker
+			ter waarde van 10 procent op kan leveren. Deze weekjoker functioneert als een positieve joker
+			en zal het dispuut 10 procent extra punten geven op het totaal aantal bierdopjes dat behaald
+			worden tijdens het eindspel op vrijdag.
+		</p>
+	</div>
+</div>
 
 <style>
-    h1{
-        text-align: center;
+	.container {
+		margin: auto;
+        max-width: 900px;
+	}
+
+    .day, .week{
+        margin: 1rem;
     }
+
+	h1 {
+		text-align: center;
+		margin: 2rem;
+	}
+
+
+    ol, ul{
+        padding-left: 1.8em;
+    }
+
+    span{
+        font-weight: bold;
+    }
+
+
+    h2, h3{
+        margin: 1.4rem 0;
+    }
+
 </style>
